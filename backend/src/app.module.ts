5f6914f30bb7a3c6d163bfdcf186bf467579cdcd
paranoica <mitjumper@gmail.com>
2025-10-30 16:32:01 +0300

    Initalized Dockerfiles & docker-compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,7 +17,7 @@ import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
             imports: [ConfigModule],
             useFactory: (configService: ConfigService) => ({
                 type: "postgres",
-                host: configService.get("DB_HOST", "postgres"),
+                host: configService.get("DB_HOST", "db"),
                 port: configService.get("DB_PORT", 5432),
                 username: configService.get("DB_USERNAME", "postgres"),
                 password: configService.get("DB_PASSWORD", "postgres"),
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     }); app.setGlobalPrefix("api");
 
     const port = process.env.PORT || 5173;
-    await app.listen(port);
+    await app.listen(port, "0.0.0.0");
 
     console.log(`Application is running on: http://localhost:${port}/api`);
 }
